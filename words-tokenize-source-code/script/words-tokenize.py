d2a0bee04b0db2c8c7c691baf4e7b521738029a1
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-11-19 10:15:29 -0300

    CHANGE VARIABLE NAME

--- words-tokenize-source-code/script/words-tokenize.py
+++ words-tokenize-source-code/script/words-tokenize.py
@@ -3,9 +3,9 @@ from nltk.tokenize import RegexpTokenizer
 
 nltk.download('punkt')
 
-def tokenize_file(file_path):
+def tokenize_file(path_input):
     try:
-        with open(file_path, 'r', encoding='utf-8') as file_input:
+        with open(path_input, 'r', encoding='utf-8') as file_input:
             lines = file_input.readlines()
             text = ''
             for line in lines:
@@ -17,24 +17,24 @@ def tokenize_file(file_path):
             tokens = tokenizer.tokenize(text)
             return tokens
     except FileNotFoundError:
-        print(f"Arquivo {file_path} não encontrado.")
+        print(f"Arquivo {path_input} não encontrado.")
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
         return None
 
-def save_output(tokens, output_path='./output/out.txt'):
+def save_output(tokens, path_output='./output/out.txt'):
     try:
-        with open(output_path, 'w', encoding='utf-8') as file_output:
+        with open(path_output, 'w', encoding='utf-8') as file_output:
             for token in tokens:
                 file_output.write(str(token) + '\n')
         print("Saída salva com sucesso.")
     except Exception as e:
         print(f"Ocorreu um erro ao salvar a saída: {e}")
 
-file_path = './input/in.txt'
-output_path = './output/out.txt'
+path_input = './input/in.txt'
+path_output = './output/out.txt'
 
-tokens = tokenize_file(file_path)
+tokens = tokenize_file(path_input)
 
 if tokens is not None:
-    save_output(sorted(tokens), output_path)
+    save_output(sorted(tokens), path_output)
