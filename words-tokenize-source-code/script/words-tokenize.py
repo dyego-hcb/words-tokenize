384fd145a92f3292ca5b31a61434efbfc4aa548b
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-11-19 10:06:50 -0300

    CORRECAO CODIGO PARA TRATAMENTO DE QUEBRA DE PALAVRAS CORRETO E TRATAMENTO DE EXECOES

--- words-tokenize-source-code/script/words-tokenize.py
+++ words-tokenize-source-code/script/words-tokenize.py
@@ -4,18 +4,37 @@ from nltk.tokenize import RegexpTokenizer
 nltk.download('punkt')
 
 def tokenize_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as fileInput:
-        text = fileInput.read()
-        text = text.replace('\n', ' ')
-        tokenizer = RegexpTokenizer(r'\w+')
-        tokens = tokenizer.tokenize(text)
-        return tokens
-    
-def save_output(tokens):
-    with open('./output/out.txt', 'w', encoding='utf-8') as fileOutput:
-        for token in tokens:
-            fileOutput.write(str(token) + '\n')
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file_input:
+            lines = file_input.readlines()
+            text = ''
+            for line in lines:
+                if line.endswith('-\n'):
+                    text += line.strip()[:-1]
+                else:
+                    text += line.strip() + ' '
+            tokenizer = RegexpTokenizer(r'\w+')
+            tokens = tokenizer.tokenize(text)
+            return tokens
+    except FileNotFoundError:
+        print(f"Arquivo {file_path} não encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+        return None
+
+def save_output(tokens, output_path='./output/out.txt'):
+    try:
+        with open(output_path, 'w', encoding='utf-8') as file_output:
+            for token in tokens:
+                file_output.write(str(token) + '\n')
+        print("Saída salva com sucesso.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao salvar a saída: {e}")
 
 file_path = './input/in.txt'
+output_path = './output/out.txt'
+
 tokens = tokenize_file(file_path)
-save_output(sorted(tokens))
+
+if tokens is not None:
+    save_output(sorted(tokens), output_path)
